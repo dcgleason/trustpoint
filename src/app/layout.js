98cd5d59f8f20f153e49bf2c1da24d07f9ff2747
Danny Gleason <danny.c.gleason@gmail.com>
2024-08-28 12:02:06 -0400

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Trustpoint Consulting | AI Integration for Enterprise SaaS",
-  description: "Unlock your data's full potential with Trustpoint Consulting. We deliver AI-powered solutions for enterprise SaaS platforms. Transform your business securely.",
+  title: "Trustpoint Consulting | AI solutions for start-ups and small businesses",
+  description: "Unlock your data's full potential with Trustpoint Consulting. We deliver AI-powered solutions for start-ups and SMB's.",
 };
 
 export default function RootLayout({ children }) {
