91df3c84ba63c7ac95de35d9f44264d876a2f3ae
Danny Gleason <danny.c.gleason@gmail.com>
2024-10-10 09:59:47 -0400

    update favocin

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,11 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Trustpoint Consulting | AI to market your business",
-  description: "AI to market your business. We're a team of AI developers and business experts who help series B through D companies market more effectively with AI.",
+  title: "Trustpoint Consulting | Custom-built marketing AI for your business",
+  description: "Custom-built AI to market your business. We're a team of AI developers and business experts who help series B through D companies market more effectively with AI.",
+  openGraph: {
+    images: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({ children }) {
