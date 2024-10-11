b13b600a5d5d2eadff59a0e971a167ec6e89778e
Danny Gleason <danny.c.gleason@gmail.com>
2024-10-11 10:00:01 -0400

    update faq

--- src/app/page.js
+++ src/app/page.js
@@ -240,7 +240,7 @@ export default function TrustpointConsultingLandingPage() {
               },
               {
                 question: "What kind of support do you offer?",
-                answer: "We offer both training your technical team to support the AI chatbot & AI agents, or we can offer ongoing technical support for you."
+                answer: "We offer both training your technical team to support the AI chatbot & AI agents, and / or we can offer ongoing technical support and feature enhancements for you."
               },
               {
                 question: "What are the costs?",
